37af5f0e58f8e82a9abeb3d891b64ba9c74266ac
Pete R <peachananr@gmail.com>
2021-03-22 10:56:36 +0700

    Fix more

--- lib/locomotive/steam/middlewares/cache.rb
+++ lib/locomotive/steam/middlewares/cache.rb
@@ -87,6 +87,7 @@ module Locomotive::Steam
           end
         end
         key = "#{Locomotive::Steam::VERSION}/site/#{site._id}/#{get_updated_at.to_i}/page/#{path}/#{query}"
+        puts key
         Digest::MD5.hexdigest(key)
       end
 
